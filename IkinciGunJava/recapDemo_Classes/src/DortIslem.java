3b3df373c950d7442a587a5de1112b14a5331168
İsmet SAYIN <98154124+rRayiha@users.noreply.github.com>
2022-10-07 11:49:52 +0300

    Add files via upload

---
+++ IkinciGunJava/recapDemo_Classes/src/DortIslem.java
@@ -0,0 +1,17 @@
+public class DortIslem {
+    public static int Topla(int sayi1, int sayi2){
+        return sayi1 + sayi2;
+    }
+
+    public static int Cikar(int sayi1, int sayi2){
+        return sayi1 - sayi2;
+    }
+
+    public static int Carpma(int sayi1, int sayi2){
+        return sayi1 * sayi2;
+    }
+
+    public static int Bolme(int sayi1, int sayi2){
+        return sayi1 / sayi2;
+    }
+}
